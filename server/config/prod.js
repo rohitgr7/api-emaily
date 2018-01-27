32231f99fafebd840999d3987363c66c9e2b4765
Rohit Gupta <30778939+rohitgr7@users.noreply.github.com>
2018-01-27 20:02:38 +0530

    Typo error

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
-  mongoURI: process.env.MONGO_URI
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
   sessionKey: process.env.SESSION_KEY
 };
