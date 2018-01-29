e7466d4ff8bac091da9260585d0083fa7191fe5b
Rohit Gupta <30778939+rohitgr7@users.noreply.github.com>
2018-01-29 17:24:17 +0530

    Setup frontend server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 require('./db/connectDb');
 require('./models/user');
@@ -33,6 +34,14 @@ app.use(passport.session());
 // Routes
 app.use('/auth', auth);
 app.use('/api', api);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Port
 const port = process.env.PORT || 5000;
